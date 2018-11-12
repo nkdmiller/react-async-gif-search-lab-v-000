8176190f3328c60edfbb1ce29ce94a746fa14f3f
Nate Miller <natekmiller96@gmail.com>
2018-11-12 18:36:20 +0000

    automatically backed up by learn

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -10,3 +10,15 @@ import React, { Component } from "react";
       event.preventDefault()
       this.props.fetchGifs(this.state.query)
     }
+
+  render(){
+    return(
+      <form onSubmit={event => this.handleSubmit(event)}>
+        <input
+          type="text"
+          value={this.state.query}
+          onSubmit={this.handleSubmit}
+        />
+      </form>      
+    )
+  }
