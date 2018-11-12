8569930822f2fa3aee14f2dbc8f2934e8988a2a5
Nate Miller <natekmiller96@gmail.com>
2018-11-12 18:20:23 +0000

    automatically backed up by learn

---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import GifList from "../components/GifList";
+import GifSearch from "../components/GifSearch";
+ class GifListContainer extends Component {
+  state = {
+    gifs: []
+  };
